608926ac24b3af95086f4b7a3e6a3812d51ae7df
Anton Stoychev <antitoxic@gmail.com>
2016-06-05 02:29:08 +0300

    Make it work with hot reloading

--- src/devtools.js
+++ src/devtools.js
@@ -9,9 +9,41 @@ export const ActionTypes = {
     MOBX_UPDATE: 'MOBX_UPDATE'
 }
 
+const devtoolRegistry = [];
+
+function getStoreAndDevTools(props) {
+    const {observable, hydrate, serialize, enhancers, children} = props;
+    for (let i = 0; i < devtoolRegistry.length; i++) {
+        let registration = devtoolRegistry[i];
+        // one observable can only have 1 devtool enabled
+        if (registration.observable == observable) {
+            return registration.result;
+        }
+    }
+    const ReduxDevTools = createDevTools(
+        <MobXMonitor {...{hydrate, observable}}>
+                     {children}
+        </MobXMonitor>
+    );
+    const enhancer = compose.apply(this, [ReduxDevTools.instrument()].concat(enhancers))
+    const store = createStore(function (state={}, action) {
+        if (action.type != ActionTypes.MOBX_UPDATE) return state;
+        return action.state
+    }, serialize(observable), enhancer);
+    reaction(() => serialize(observable), state => {
+        if (observable.$mobx.fromJS) {
+            observable.$mobx.fromJS = false;
+            return;
+        }
+        store.dispatch({type: ActionTypes.MOBX_UPDATE, state})
+    });
+    const result = [store, ReduxDevTools];
+    devtoolRegistry.push({result, observable});
+    return result
+};
+
 export default class DevTools extends Component {
-    static instrument;
-    static ReduxDevTools;
+    ReduxDevTools;
 
     static propTypes = {
         hydrate: PropTypes.func.isRequired,
@@ -30,24 +62,8 @@ export default class DevTools extends Component {
 
     constructor(props, context) {
         super(props, context);
-        const {observable, hydrate, serialize, enhancers, children} = props;
-        const ReduxDevTools = createDevTools(
-            <MobXMonitor {...{hydrate, observable}}>
-                {children}
-            </MobXMonitor>
-        );
-        const enhancer = compose.apply(this, [ReduxDevTools.instrument()].concat(enhancers))
-        this.store = createStore(function (state={}, action) {
-            if (action.type != ActionTypes.MOBX_UPDATE) return state;
-            return action.state
-        }, serialize(observable), enhancer);
-        reaction(() => serialize(observable), state => {
-            if (observable.$mobx.fromJS) {
-                observable.$mobx.fromJS = false;
-                return;
-            }
-            this.store.dispatch({type: 'MOBX_UPDATE', state})
-        });
+        const [store, ReduxDevTools] = getStoreAndDevTools(props);
+        this.store = store;
         this.ReduxDevTools = ReduxDevTools;
     }
 
